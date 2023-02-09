c6d33e45f364ce913a3c464ad3593adbac3753b8
Derek Duncan <dduncan@atlassian.com>
2023-02-09 12:28:37 +1100

    test

--- internal/hooks/http_auth.go
+++ internal/hooks/http_auth.go
@@ -2,6 +2,9 @@ package hooks
 
 import (
 	"bytes"
+	"context"
+	"fmt"
+	"strconv"
 
 	"cloud.google.com/go/logging"
 	"github.com/mochi-co/mqtt/v2"
@@ -9,12 +12,13 @@ import (
 )
 
 type HTTPAuthHook struct {
+	Client *api.HTTPAuthBackendClient
 	Logger *logging.Logger
 	mqtt.HookBase
 }
 
 func (h *HTTPAuthHook) ID() string {
-	return "firebase-auth-hook"
+	return "http-auth-hook"
 }
 
 func (h *HTTPAuthHook) Provides(b byte) bool {
@@ -25,14 +29,30 @@ func (h *HTTPAuthHook) Provides(b byte) bool {
 }
 
 func (h *HTTPAuthHook) Init(config any) error {
-	h.Logger.StandardLogger(logging.Debug).Println("initialized httpauth")
+	httpauthclient, err := api.NewClient(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	h.Client = httpauthclient
+
 	return nil
 }
 
 func (h *HTTPAuthHook) OnConnectAuthenticate(cl *mqtt.Client, pk packets.Packet) bool {
-	return true
+	allowed, err := h.Client.CheckClientAuth(context.Background(), cl.ID, string(pk.Connect.Username), string(pk.Connect.Password))
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return allowed
 }
 
 func (h *HTTPAuthHook) OnACLCheck(cl *mqtt.Client, topic string, write bool) bool {
-	return true
+	allowed, err := h.Client.CheckClientACLs(context.Background(), cl.ID, string(cl.Properties.Username), topic, strconv.FormatBool(write))
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return allowed
 }
