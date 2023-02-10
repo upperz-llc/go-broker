b59f27ed3333c40c7399f53b9d30779c81fc9e28
Derek Duncan <derekduncan@gmail.com>
2023-02-10 21:31:49 +1100

    test

--- internal/admin/secretmanager.go
+++ internal/admin/secretmanager.go
@@ -9,22 +9,11 @@ import (
 )
 
 func getAdminCredentials(ctx context.Context) (string, error) {
-	fmt.Println("getAdminCredentials")
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
 		return "", fmt.Errorf("failed to create secretmanager client: %v", err)
 	}
 	defer client.Close()
-
-	// secret, err := client.GetSecret(ctx, &secretmanagerpb.GetSecretRequest{
-	// 	Name: "projects/481474188273/secrets/BROKER_ADMIN",
-	// })
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println(secret.String())
-	// secret.
 
 	resp, err := client.AccessSecretVersion(ctx, &secretmanagerpb.AccessSecretVersionRequest{
 		Name: "projects/481474188273/secrets/BROKER_ADMIN/versions/latest",
