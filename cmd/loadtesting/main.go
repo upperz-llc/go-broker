f91adc3f6dfad6240aefe092c76e9ebfc81ddd8b
Derek Duncan <dduncan@atlassian.com>
2023-06-20 11:16:19 -0500

    Add load testing docker

---
+++ cmd/loadtesting/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	zlg "github.com/mark-ignacio/zerolog-gcp"
+	"github.com/mochi-co/mqtt/v2"
+	"github.com/mochi-co/mqtt/v2/hooks/auth"
+	"github.com/mochi-co/mqtt/v2/hooks/debug"
+	"github.com/mochi-co/mqtt/v2/listeners"
+	"github.com/rs/zerolog"
+	"github.com/upperz-llc/go-broker/internal/webserver"
+)
+
+func main() {
+	ctx := context.Background()
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	// Create the new MQTT Server.
+	server := mqtt.New(&mqtt.Options{})
+	l := server.Log.Level(zerolog.DebugLevel)
+	server.Log = &l
+
+	// ****************** CONFIGURE LOGGING ************
+
+	// pull project id from env
+	pid, found := os.LookupEnv("GCP_PROJECT_ID")
+	if !found {
+		log.Fatal("GCP_PROJECT_ID not found")
+	}
+
+	// Create GCP Zap Logger
+	gcpWriter, err := zlg.NewCloudLoggingWriter(ctx, pid, "mochi-broker", zlg.CloudLoggingOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gcpZeroLogger := zerolog.New(gcpWriter)
+	debugLogger := gcpZeroLogger.Level(zerolog.DebugLevel)
+	server.Log = &debugLogger
+
+	_ = server.AddHook(new(debug.Hook), &debug.Options{})
+	_ = server.AddHook(new(auth.AllowHook), nil)
+
+	// Create a TCP listener on a standard port.
+	tcp := listeners.NewTCP("t1", ":1883", nil)
+
+	// Create a healthcheck listener
+	hc := listeners.NewHTTPHealthCheck("healthcheck", ":8080", nil)
+
+	err = server.AddListener(tcp)
+	if err != nil {
+		server.Log.Err(err).Msg("")
+		return
+	}
+
+	err = server.AddListener(hc)
+	if err != nil {
+		server.Log.Err(err).Msg("")
+		return
+	}
+
+	go func() {
+		err := server.Serve()
+		if err != nil {
+			server.Log.Err(err).Msg("")
+			return
+		}
+	}()
+
+	go webserver.StartWebServer(server)
+
+	<-done
+	server.Log.Warn().Msg("caught signal, stopping...")
+	server.Close()
+	server.Log.Info().Msg("main.go finished")
+
+}
